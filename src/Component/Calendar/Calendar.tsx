fa20b52d2fb313f56f431ef6a8575321056dfef6
VengateshGV <53400321+vengateshkarthick@users.noreply.github.com>
2023-12-09 17:05:04 +0000

    Fixed the size

--- src/Component/Calendar/Calendar.tsx
+++ src/Component/Calendar/Calendar.tsx
@@ -7,7 +7,7 @@ import './calendar.scss'
 
 const weekdays: Mweekdays['short'] = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"];
 
-function MCalendar({ events, onSelectDate }:MCalendar) {
+function Calendar({ events, onSelectDate }:MCalendar) {
     const currentDate = new Date();
     const [selectedDate, setSelectedDate] = React.useState<{slcdate: string, events?:[] | MEvents[]}>({slcdate: ''});
     const startDate = _dutlis.startOfMonth(currentDate);
@@ -39,23 +39,23 @@ function MCalendar({ events, onSelectDate }:MCalendar) {
           className='mcalendar text-center'
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
-          transition={{ delay: 0.3, duration: 0.1 }}
+          transition={{ delay: 0.1, duration: 0.3 }}
 
         >
           {weekdays.map((day, index) => (<motion.article className='mx-1 py-1' key={`mcalendar-day${index}`}>{day}</motion.article>))}
           {bufferDays.map((bfd) => <motion.article key={bfd + '_empty'} className='mcalendar-date'>&nbsp;</motion.article>)}
           {daysInTheMonth.map((date) => {
-            const formattedDate = _dutlis.format(date, "DD/MM/YYY");
+            const formattedDate = _dutlis.format(date, "dd/mm/yyyy");
             const displayDate = _dutlis.format(date, "d");
-            const eventsLength = groupedEvents[formattedDate].length;
+            const eventsLength = groupedEvents[formattedDate]?.length;
 
             return (
                 <motion.article
                 initial={{ scale: 0.9 }}
                 animate={{ scale: 1 }}
-                whileTap={{ scale: 1.1 }}
-                whileHover={{ scale: 1.1 }}
-                transition={{ delay: 0.2, duration: 0.1 }}
+                whileTap={{ scale: 1.5 }}
+                whileHover={{ scale: 1.5 }}
+                transition={{ duration: 0.2, type: 'spring' }}
                 className={`mcalendar-date d-flex flex-column mx-1 py-1 ${selectedDate.slcdate === formattedDate ? 'selected' : ''}`}
                 key={`mcalendar-date-${displayDate}`}
                 data-date={formattedDate}
@@ -75,4 +75,4 @@ function MCalendar({ events, onSelectDate }:MCalendar) {
 
 }
 
-export default MCalendar;
+export default Calendar;
